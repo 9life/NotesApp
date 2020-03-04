57c3014ba4baf5e95efb7111e6b19a24eaa5217d
Olga <9life@mail.ru>
2020-03-04 21:59:29 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { tsPropertySignature } from '@babel/types';
 
-function App() {
+function Note(props) {
+  const style = {backgroundColor: props.color}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='note' style={style}>
+      {props.children}
     </div>
   );
 }
 
-export default App;
+function NotesGrid(props) {
+  return (
+    <div className='notes-grid'>
+     {props.notes.map((note) => <Note key={note.id} color={note.color}>{note.text}</Note> )}
+    </div>
+  );
+}
+
+function NotesEditor() {
+  return (
+    <div className='note-editor'>
+      NotesEditor
+    </div>
+  );
+}
+
+
+function NotesApp() {
+  const notes = [
+    {
+        id: 0,
+        text: "note 1",
+        color: "#FFD700"
+    },
+    {
+        id: 1,
+        text: "note 2 note 2",
+        color: "#20B2AA"
+    },
+    {
+        id: 2,
+        text: "note 3 note 3 note 3",
+        color: "#FFA700"
+    },
+    {
+        id: 3,
+        text: "note 4",
+        color: "#87CEAF"
+    },
+    {
+        id: 4,
+        text: "note 5",
+        color: "#87CEFA"
+    }
+];
+  return (
+    <div className="notes-app">
+      NotesApp
+<NotesEditor />
+      <NotesGrid notes={notes}/>
+    </div>
+  );
+}
+
+export default NotesApp;
